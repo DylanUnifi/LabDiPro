b22b17c97ed407d1489a043450529b2e4d9715c0
DylanUnifi <dylan.fouepe@stud.unifi.it>
2020-07-17 17:25:43 +0200

    Test of the class messageTest

--- test/messageTest.cpp
+++ test/messageTest.cpp
@@ -2,3 +2,17 @@
 // Created by FOUEPE on 17/07/2020.
 //
 
+#include "gtest/gtest.h"
+#include "../message.h"
+
+
+TEST(Message, GetterSetter) {
+    message mes("Maldini", "Yann", "Hello, what's up Yann ?");
+    ASSERT_EQ(mes.getSender(), "Maldini");
+    ASSERT_EQ(mes.getReceiver(), "Yann");
+    ASSERT_EQ(mes.getText(), "Hello, what's up Yann ?");
+    ASSERT_EQ(mes.isRead(), false);
+    mes.setRead(true);
+    ASSERT_EQ(mes.isRead(), true);
+
+}
--- test/runAllTests.cpp
+++ test/runAllTests.cpp
@@ -2,3 +2,10 @@
 // Created by FOUEPE on 17/07/2020.
 //
 
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
