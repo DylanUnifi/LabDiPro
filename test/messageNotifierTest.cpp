56b36111b3429788f579b8fa90ffa159d1c1e077
DylanUnifi <dylan.fouepe@stud.unifi.it>
2020-07-17 17:28:42 +0200

    Test of the class messageNotifierTest.cpp

--- test/messageNotifierTest.cpp
+++ test/messageNotifierTest.cpp
@@ -2,3 +2,16 @@
 // Created by FOUEPE on 17/07/2020.
 //
 
+#include "gtest/gtest.h"
+#include "../messageNotifier.h"
+
+TEST(MessageNotifier, isActive) {
+    user a("Maldini");
+    user b("Yann");
+    chat *c = new chat(a,b);
+    std::shared_ptr<chat> ptr = std::make_shared<chat>(*c);
+    messageNotifier m(true,ptr);
+    ASSERT_TRUE(m.isActive());
+    m.setActive(false);
+    ASSERT_FALSE(m.isActive());
+}
