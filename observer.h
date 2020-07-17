27d077a42461fc572864465b9690769ff67edcb6
DylanUnifi <dylan.fouepe@stud.unifi.it>
2020-07-17 16:33:07 +0200

    classe abstratta observer

---
+++ observer.h
@@ -0,0 +1,14 @@
+//
+// Created by FOUEPE on 17/07/2020.
+//
+
+#ifndef LABDIPRO_OBSERVER_H
+#define LABDIPRO_OBSERVER_H
+class Observer {
+public:
+    virtual ~Observer(){}
+    virtual void attach() = 0;
+    virtual void detach() = 0;
+    virtual void update() = 0;
+};
+#endif //LABDIPRO_OBSERVER_H
