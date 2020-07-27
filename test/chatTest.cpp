0b1b906d2246b89e8d70cd4489c5b7f918b860a7
DylanUnifi <dylan.fouepe@stud.unifi.it>
2020-07-27 10:55:38 +0200

    Modify chat test class

--- test/chatTest.cpp
+++ test/chatTest.cpp
@@ -24,17 +24,21 @@ TEST(chat,GetterSetter){
 
 TEST(chat,functions){
     chat c (maldini, yann);
-    message mes ("Maldini","Yann", "Ciao Yann, come va?");
-    c.addMessage (mes);
-    c.addMessage (mes);
-    c.addMessage (mes);
-    c.addMessage (mes);
-    c.addMessage (mes);
+    message mes1 ("Maldini","Yann", "Ciao Yann, come va?");
+    c.addMessage (mes1);
+    message mes2 ("Yann","Maldini", "Tutto bene e tu ?");
+    c.addMessage (mes2);
+    message mes3 ("Maldini","Yann", "Niente male, ma ti volevo chiedere....");
+    c.addMessage (mes3);
+    message mes4 ("Yann","Maldini", "Dimmi tutto.");
+    c.addMessage (mes4);
+    message mes5 ("Maldini","Yann", "Come è andato l'esame quesa mattina ?");
+    c.addMessage (mes5);
 
     /* ASSERT_EQ(c.lastMessage(), mes);*/
-    c.readMessage(3);
     c.readMessage(1);
     c.readMessage(2);
+    c.readMessage(3);
     ASSERT_EQ(c.getUnreadMessages(), 2);
     ASSERT_THROW(c.readMessage(5), std::out_of_range);
     ASSERT_THROW(c.readMessage(6), std::out_of_range);
